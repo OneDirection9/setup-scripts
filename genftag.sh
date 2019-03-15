04f97513849b27e65cf94bfd807cfa20df451bfc
Zhipeng Han <hanzhipeng9@gmail.com>
2019-03-15 21:49:54 +0800

    not find .pyc
    Signed-off-by: Zhipeng Han <hanzhipeng9@gmail.com>

--- genftag.sh
+++ genftag.sh
@@ -1,4 +1,4 @@
 # generate filename tags for lookupfile plugin
 (echo "!_TAG_FILE_SORTED        2       /2=foldcase/";
-(find . -not -name '*.o' -not -wholename '*.svn*' -not -wholename '*.git*' -not -regex '.*\.\(png\|gif\)' -type f -printf "%f\t%p\t1\n" | \
+(find . -not -name '*.o' -not -wholename '*.svn*' -not -wholename '*.git*' -not -wholename '*.pyc*' -not -regex '.*\.\(png\|gif\)' -type f -printf "%f\t%p\t1\n" | \
 sort -f)) > filename.tags
