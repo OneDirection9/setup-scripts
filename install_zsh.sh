7588baadeb7993cd11be0240cbf551b21d74a13c
Zhipeng Han <hanzhipeng9@gmail.com>
2019-07-10 21:30:11 +0800

    [fix]: change plugins' url
    Signed-off-by: Zhipeng Han <hanzhipeng9@gmail.com>

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,5 +1,5 @@
 # Install plugins
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
 cp zshrc ~/.zshrc
