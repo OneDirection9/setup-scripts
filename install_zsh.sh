82a461fbeae96ab42f299e2027c6f5b648c4f04a
Zhipeng Han <hanzhipeng9@gmail.com>
2021-04-18 22:48:42 -0700

    Fix zsh plugins path
    Signed-off-by: Zhipeng Han <hanzhipeng9@gmail.com>

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,8 +1,8 @@
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 # Install plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ${HOME}/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${HOME}/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${HOME}/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${HOME}/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 cp zshrc ~/.zshrc
 
